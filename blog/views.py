496659353dd5bd27637bd8f892679ee9993464e0
xFingerlinKx <apanasov86@mail.ru>
2019-03-09 22:08:40 +0300

    Class Based Views (CBVs) и использование Миксинов

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 
 from .models import Post, Tag
+
+from .utils import ObjectDetailMixin
 
 
 def posts_list(request):
@@ -8,9 +11,18 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+# Class Based Views (CBVs) и использование Миксинов
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+    # def get(self, request, slug):
+    #     # post = Post.objects.get(slug__iexact=slug)
+    #     post = get_object_or_404(Post, slug__iexact=slug)
+    #     return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 def tags_list(request):
@@ -18,6 +30,15 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+# Class Based Views (CBVs) и использование Миксинов
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+    # def get(self, request, slug):
+    #     # tag = Tag.objects.get(slug__iexact=slug)
+    #     tag = get_object_or_404(Tag, slug__iexact=slug)
+    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
